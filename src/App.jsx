f8825f7bdce843350fa9cbc9ef73105e509014c6
viniciusP23 <vinicius_pereira1@live.com>
2026-05-19 22:48:55 -0300

    adiciona estrutura inicial do app com rotas e páginas

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
+  )
+}
+
+export default App
+
---
+++ src/components/pages/Company.jsx
@@ -0,0 +1,10 @@
+function Company() {
+    return (
+        <>
+            <h1>Company</h1>
+        </>
+    )
+    
+}
+
+export default Company

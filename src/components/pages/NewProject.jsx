98890cb578f79b3e2486ec90127c45141deb64ac
viniciusP23 <vinicius_pereira1@live.com>
2026-05-22 21:54:23 -0300

    adiciona formulário de criação de projetos

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,8 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
+
 function NewProject() {
     return (
-        <>
-            <h1>NewProject</h1>
-        </>
+        <div className={styles.newproject_container }>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
     )
 }
 

8857f1cab4a3cb01a00787212a00987be5f1b285
viniciusP23 <vinicius_pereira1@live.com>
2026-05-25 21:11:15 -0300

    estilizando formulário de criação de projetos

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,21 +1,16 @@
-function ProjectForm() {
+import Input from '../Form/input'
+import Select from '../Form/Select'
+import SubmitButton from '../Form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
 
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="Nome" placeholder="Insira o nome do projeto" />
+            <Input type="number" text="Orçamento do projeto" name="Budget" placeholder="Insira o orçamento total" />
+            <Select name="category_id" text="Selecione a categoria"/>
+            <SubmitButton text={btnText}/>
         </form>
     )
 }
